1ff68484cf064735740393dcc49e306159241cfe
Ade Widyatama DB <adewidyatamadb@gmail.com>
2021-08-03 11:35:59 +0800

    Optimizing the template cache function using wide application setting config

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -7,28 +7,38 @@ import (
 	"log"
 	"net/http"
 	"path/filepath"
+
+	"github.com/adewidyatamadb/GoLang-Learn/pkg/config"
 )
 
 var functions = template.FuncMap{}
+var app *config.AppConfig
+
+// NewTemplates sets the config for the template package
+func NewTemplates(a *config.AppConfig) {
+	app = a
+}
 
 // RenderTemplate renders templates using html/template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	// Get the template cache from the app config
-	tc, err := CreateTemplateCache()
-	if err != nil {
-		log.Fatal(err)
+	var tc map[string]*template.Template
+	if app.UseCache {
+		// Get the template cache from the app config
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CreateTemplateCache()
 	}
 
 	t, ok := tc[tmpl]
 	if !ok {
-		log.Fatal(err)
+		log.Fatal("could not get template from template cache")
 	}
 
 	buf := new(bytes.Buffer)
 
 	_ = t.Execute(buf, nil)
 
-	_, err = buf.WriteTo(w)
+	_, err := buf.WriteTo(w)
 	if err != nil {
 		fmt.Println("error writing template into browser:", err)
 	}
