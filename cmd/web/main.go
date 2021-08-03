5883282719820c49f798983ba852cdf0e32affcf
Ade Widyatama DB <adewidyatamadb@gmail.com>
2021-08-03 11:10:35 +0800

    Setting application wide configuration

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/adewidyatamadb/GoLang-Learn/pkg/config"
 	"github.com/adewidyatamadb/GoLang-Learn/pkg/handlers"
+	"github.com/adewidyatamadb/GoLang-Learn/pkg/render"
 )
 
 const portNumber = ":8080"
@@ -13,6 +16,14 @@ var server = "localhost"
 
 //main is the main application function
 func main() {
+	var app config.AppConfig
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -13,6 +13,7 @@ var functions = template.FuncMap{}
 
 // RenderTemplate renders templates using html/template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
+	// Get the template cache from the app config
 	tc, err := CreateTemplateCache()
 	if err != nil {
 		log.Fatal(err)
