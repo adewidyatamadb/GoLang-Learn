3e978baf5a8230a0df4963ab8a751d008beaf8c8
Ade Widyatama DB <adewidyatamadb@gmail.com>
2021-08-03 10:04:58 +0800

    Reorganizing the code into their respective folder

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/adewidyatamadb/GoLang-Learn/pkg/handlers"
 )
 
 const portNumber = ":8080"
@@ -11,8 +13,8 @@ var server = "localhost"
 
 //main is the main application function
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	fmt.Println(fmt.Sprintf("Starting application on %s%s", server, portNumber))
 	_ = http.ListenAndServe(server+portNumber, nil)
